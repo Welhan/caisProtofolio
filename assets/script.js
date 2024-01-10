2835d3fc467f6f5a1825fd7b997306a2b3e7ec33
Welhan <welhan.susanto@gmail.com>
2024-01-10 23:19:29 +0700

    go to last page and back to first page

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,47 @@ pageTurnBtn.forEach((el, index) => {
     }
   };
 });
+
+// contact us button when click
+
+const pages = document.querySelectorAll(".book-page.page-right");
+const contactBtn = document.querySelector(".btn.contact-us");
+
+contactBtn.onclick = () => {
+  pages.forEach((page, index) => {
+    setTimeout(() => {
+      page.classList.add("turn");
+
+      setTimeout(() => {
+        page.style.zIndex = 20 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+// back to first page btn click
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+  pageNumber--;
+  if (pageNumber < 0) {
+    pageNumber = totalPages - 1;
+  }
+}
+
+const backBtn = document.querySelector(".back-first-page");
+
+backBtn.onclick = () => {
+  pages.forEach((_, index) => {
+    setTimeout(() => {
+      reverseIndex();
+      pages[pageNumber].classList.remove("turn");
+
+      setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].style.zIndex = 10 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
